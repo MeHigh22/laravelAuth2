c6a62a2afbe3b5a7a27856f35b2c607fba981ff6
MeHigh22 <72247500+MeHigh22@users.noreply.github.com>
2021-09-07 11:22:26 +0200

    Fait Auth

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/dashboardexo', function () {
+    return view('dashboardexo');
+})->middleware(['auth'])->name('dashboardexo');
+
+
 require __DIR__.'/auth.php';
